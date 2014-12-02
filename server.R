b36b6446d74151f93b88fbb63d4ab907c394255b
Beryl <beryl.c.zhuang@gmail.com>
2014-12-01 20:18:39 -0800

    working-before adding date range; add data

--- server.R
+++ server.R
@@ -1,31 +1,46 @@
+# load packages
 library(ggplot2)
-gdURL <- "http://tiny.cc/gapminder"
-gDat <- read.delim(file = gdURL) 
+
+#source helper funcitons
+
+source("helper_functions.R")
+
+# load data
+gDat <- readRDS("data/gapminder.rds")
+
+
+
 
 shinyServer(function(input, output) {
 	
-	# Drop-down selection box generated from Gapminder dataset
+	#1. Drop-down selection box generated from Gapminder dataset    #ui.R: uiOutput("choose_country")
 	output$choose_country <- renderUI({
 		selectInput("country_from_gapminder", "Country", as.list(levels(gDat$country)))
 	})
 	
+	#2 reactive input from user (pick country to display) and subset the table for tabke display and plots
 	one_country_data <- reactive({
 		if(is.null(input$country_from_gapminder)) {
 			return(NULL)
 		}
 		subset(gDat, country == input$country_from_gapminder &
-					 	year >= input$year_range[1] & year <= input$year_range[2] )
+					 	year >= input$year_range[1] & year <= input$year_range[2] )       # see ui.R sliderInput for year_range
 	})
 	
+	#3 render the table of selected contents          #ui.R : tableOutput("gapminder_table")
 	output$gapminder_table <- renderTable({ 
 		one_country_data()
 	})
+	
+	#4 render text output of the selected country   #ui.R: textOutput("output_country")
 	output$output_country <- renderText({
 		if (is.null(input$country_from_gapminder)){
 			return(NULL)
 		}
 		paste("Country selected", input$country_from_gapminder)
 	})
+	
+	#5 render the plot, #ui.R: plotOutput("ggplot_gdppc_vs_country")
 	output$ggplot_gdppc_vs_country <- renderPlot({
 		if(is.null(one_country_data())) {
 			return(NULL)
@@ -34,3 +49,7 @@ shinyServer(function(input, output) {
 		p + geom_point() 
 	})
 })
+
+
+#------------------------
+#------------------------
--- ui.R
+++ ui.R
@@ -4,16 +4,16 @@ shinyUI(fluidPage(
 	sidebarLayout(
 		sidebarPanel(
 			h2("Choose country and years from Gapminder data set"),
-			uiOutput("choose_country"),
-			sliderInput("year_range", 
+			uiOutput("choose_country"),            # see #1 in server.R
+			sliderInput("year_range",              # year input for server.R
 									label = "Range of years:",
 									min = 1952, max = 2007, 
 									value = c(1955, 2005),
 									format = "####")
 		),
-		mainPanel(h3(textOutput("output_country")),
-							plotOutput("ggplot_gdppc_vs_country"),
-							tableOutput("gapminder_table")              
+		mainPanel(h3(textOutput("output_country")),            #4
+							plotOutput("ggplot_gdppc_vs_country"),      #5
+							tableOutput("gapminder_table")              #3
 		)
 	)
 ))
