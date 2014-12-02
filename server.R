f5b613fd6acb40ff6129fe4ea49f96d521afa5b2
Beryl <beryl.c.zhuang@gmail.com>
2014-12-01 20:47:36 -0800

    date widge input added, working

--- server.R
+++ server.R
@@ -18,13 +18,23 @@ shinyServer(function(input, output) {
 		selectInput("country_from_gapminder", "Country", as.list(levels(gDat$country)))
 	})
 	
-	#2 reactive input from user (pick country to display) and subset the table for tabke display and plots
+# 	#2 reactive input from user (pick country to display) and subset the table for tabke display and plots
+# 	one_country_data <- reactive({
+# 		if(is.null(input$country_from_gapminder)) {
+# 			return(NULL)
+# 		}
+# 		subset(gDat, country == input$country_from_gapminder &
+# 					 	year >= input$year_range[1] & year <= input$year_range[2] )       # see ui.R sliderInput for year_range
+# 	})
+
+#2.1 reactive input from user (pick country to display) and subset the table for tabke display and plots
 	one_country_data <- reactive({
 		if(is.null(input$country_from_gapminder)) {
 			return(NULL)
 		}
 		subset(gDat, country == input$country_from_gapminder &
-					 	year >= input$year_range[1] & year <= input$year_range[2] )       # see ui.R sliderInput for year_range
+					 	year >= as.numeric(format(input$dateRange[1], "%Y")) & 
+					 	year <= as.numeric(format(input$dateRange[2], "%Y")) )       # see ui.R dateRange, only use year as input
 	})
 	
 	#3 render the table of selected contents          #ui.R : tableOutput("gapminder_table")
@@ -48,6 +58,16 @@ shinyServer(function(input, output) {
 		p <-  ggplot(one_country_data(), aes(x = year, y = gdpPercap))
 		p + geom_point() 
 	})
+	
+	# 6 Replace the slider for the years instead with the widget dateRangeInput.
+	output$dateRangeText  <- renderText({
+		paste("input$dateRange is", 
+					paste(as.character(input$dateRange), collapse = " to ")
+		)
+	})
+	
+	
+	
 })
 
 
--- ui.R
+++ ui.R
@@ -4,16 +4,25 @@ shinyUI(fluidPage(
 	sidebarLayout(
 		sidebarPanel(
 			h2("Choose country and years from Gapminder data set"),
-			uiOutput("choose_country"),            # see #1 in server.R
-			sliderInput("year_range",              # year input for server.R
-									label = "Range of years:",
-									min = 1952, max = 2007, 
-									value = c(1955, 2005),
-									format = "####")
+			uiOutput("choose_country"),
+			
+# commented out the year slider 			
+# 			sliderInput("year_range", 
+# 									label = "Range of years:",
+# 									min = 1952, max = 2007, 
+# 									value = c(1955, 2005),
+# 									format = "####"
+# 									),
+
+# use dateRangeInput with calender  #2.1
+			dateRangeInput('dateRange',
+										 label = 'Date range input: yyyy-mm-dd',
+										 start = "1957-01-01", end = "2007-01-01"
+										 )
 		),
-		mainPanel(h3(textOutput("output_country")),            #4
-							plotOutput("ggplot_gdppc_vs_country"),      #5
-							tableOutput("gapminder_table")              #3
+		mainPanel(h3(textOutput("output_country")),
+							plotOutput("ggplot_gdppc_vs_country"),
+							tableOutput("gapminder_table")              
 		)
 	)
 ))
